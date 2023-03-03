7bf268ebcdd6a17f0e3ef655170001c8b63bd62f
Saiful Islam Sumon <125297402+SaifulSumon23@users.noreply.github.com>
2023-03-03 22:27:51 +0600

    Update First_Lab.java

--- First_Lab.java
+++ First_Lab.java
@@ -5,7 +5,7 @@ public class First_Lab {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello!");
-		System.out.println("I am Soikat");
+		System.out.println("I am Sumon");
 		
 
 	}
