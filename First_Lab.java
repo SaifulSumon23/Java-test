f777787b9cc12be754ecd1dc44745c59c5b8933f
Saiful Islam Sumon <125297402+SaifulSumon23@users.noreply.github.com>
2023-03-03 22:27:26 +0600

    Add files via upload

---
+++ First_Lab.java
@@ -0,0 +1,13 @@
+package My_Java_l;
+
+public class First_Lab {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello!");
+		System.out.println("I am Soikat");
+		
+
+	}
+
+}
